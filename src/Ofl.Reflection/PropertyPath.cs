9ed3ce49fab54e539314b8e602646d982c56b120
casperOne <casperOne@caspershouse.com>
2017-05-23 12:57:59 -0400

    Added PropertyPath.

---
+++ src/Ofl.Reflection/PropertyPath.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Ofl.Reflection
+{
+    public class PropertyPath
+    {
+        #region Constructor
+
+        internal PropertyPath() : this(null)
+        { }
+
+        internal PropertyPath(PropertyPath root)
+        {
+            // If the root is null, assign the path.
+            if (root == null)
+            {
+                // Assign path.
+                _path = new Queue<PropertyInfo>();
+
+                // Set the root.
+                Root = this;
+            }
+            else
+                // Root is root.
+                Root = root;
+
+            // Root must not be null.
+            Debug.Assert(Root != null);
+        }
+
+        #endregion
+
+        #region Instance, read-only state.
+
+        private readonly Queue<PropertyInfo> _path;
+
+        public PropertyPath Root { get; }
+
+        public IEnumerable<PropertyInfo> Path => Root._path;
+
+        #endregion
+
+        #region Action methods.
+
+        protected void Append(PropertyInfo property)
+        {
+            // Validate parameters.
+            if (property == null) throw new ArgumentNullException(nameof(property));
+
+            // Push.
+            Root._path.Enqueue(property);
+        }
+
+        public static PropertyPath<T> Of<T>()
+        {
+            // Create a new instance.
+            return new PropertyPath<T>();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Ofl.Reflection/PropertyPath[T].cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Ofl.Reflection
+{
+    public class PropertyPath<T> : PropertyPath
+    {
+        #region Constructors
+
+        internal PropertyPath() : base(null)
+        { }
+
+        internal PropertyPath(PropertyPath root) : base(root)
+        { }
+
+        #endregion
+
+        #region Helpers.
+
+        public PropertyPath<TResult> ThenEnumerable<TResult>(Expression<Func<T, IEnumerable<TResult>>> expression)
+        {
+            // Validate parameters.
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
+
+            // Get the member info.
+            var propertyInfo = (expression.Body as MemberExpression)?.Member as PropertyInfo;
+
+            // If null, throw.
+            if (propertyInfo == null)
+                throw new InvalidOperationException($"The {nameof(expression)} parameter must be an expression backed by a PropertyInfo.");
+
+            // Push.
+            Append(propertyInfo);
+
+            // Return the new expression.
+            return new PropertyPath<TResult>(Root);
+        }
+
+        public PropertyPath<TResult> Then<TResult>(Expression<Func<T, TResult>> expression)
+        {
+            // Validate parameters.
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
+
+            // Get the member info.
+            var propertyInfo = (expression.Body as MemberExpression)?.Member as PropertyInfo;
+
+            // If null, throw.
+            if (propertyInfo == null)
+                throw new InvalidOperationException($"The {nameof(expression)} parameter must be an expression backed by a PropertyInfo.");
+
+            // Push.
+            Append(propertyInfo);
+
+            // Return the new expression.
+            return new PropertyPath<TResult>(Root);
+        }
+
+        #endregion
+    }
+}
